8be1a66511263f1a891d0c4d3369ddfd121f807f
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-06-02 23:44:32 +0900

    init commit

---
+++ src/main/java/com/sparta/springnewsfeed/SpringNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewsfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNewsfeedApplication.class, args);
+    }
+
+}

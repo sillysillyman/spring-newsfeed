315a162304c23921a702c8064081050713247c26
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-05 13:28:08 +0900

    feat: PostResponse DTO 생성
    - 게시물 응답을 위한 PostResponse DTO 추가
    - 게시물 ID, 제목, 내용 필드 포함

---
+++ src/main/java/com/sparta/springnewsfeed/dto/PostResponse.java
@@ -0,0 +1,13 @@
+package com.sparta.springnewsfeed.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostResponse {
+
+    private Long id;
+
+    private String title;
+    
+    private String content;
+}

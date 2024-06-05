4b06645eaca46469c3bb86be4fee1b2cbb8fa8f6
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-05 16:48:01 +0900

    feat: PostResponse 클래스에 Post 엔티티를 이용한 생성자 추가
    - PostResponse 클래스에 Post 엔티티를 이용하여 필드 값을 초기화하는 생성자 추가

--- src/main/java/com/sparta/springnewsfeed/dto/PostResponse.java
+++ src/main/java/com/sparta/springnewsfeed/dto/PostResponse.java
@@ -1,5 +1,6 @@
 package com.sparta.springnewsfeed.dto;
 
+import com.sparta.springnewsfeed.entity.Post;
 import lombok.Getter;
 
 @Getter
@@ -8,6 +9,12 @@ public class PostResponse {
     private Long id;
 
     private String title;
-    
+
     private String content;
+
+    public PostResponse(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+    }
 }

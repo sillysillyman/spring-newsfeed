ff2b0241d89ec1d3bf89c9af210699947d833d79
soyoung <soyoung.yang94@gmail.com>
2024-06-06 14:51:54 +0900

    refactor: 패키지 리팩토링

--- src/main/java/com/sparta/springnewsfeed/common/TestDataRunner.java
+++ src/main/java/com/sparta/springnewsfeed/common/TestDataRunner.java
@@ -20,18 +20,18 @@ public class TestDataRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) {
         // 테스트 Post 생성
         // post 1
-        Post testPost1 = new Post();
-        testPost1.setUserId(1L);
-        testPost1.setTitle("Test Post111");
-        testPost1.setContent("This is a test post111");
-        testPost1 = postRepository.save(testPost1);
-
-        // post 2
-        Post testPost2 = new Post();
-        testPost2.setUserId(1L);
-        testPost2.setTitle("Test Post222");
-        testPost2.setContent("This is a test post222");
-        testPost2 = postRepository.save(testPost2);
+//        Post testPost1 = new Post();
+//        testPost1.setUserId(1L);
+//        testPost1.setTitle("Test Post111");
+//        testPost1.setContent("This is a test post111");
+//        testPost1 = postRepository.save(testPost1);
+//
+//        // post 2
+//        Post testPost2 = new Post();
+//        testPost2.setUserId(1L);
+//        testPost2.setTitle("Test Post222");
+//        testPost2.setContent("This is a test post222");
+//        testPost2 = postRepository.save(testPost2);
 
     }
 

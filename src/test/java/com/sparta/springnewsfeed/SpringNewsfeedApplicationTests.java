4093fd2f7055b6d25481413bd495b22ce0613671
soyoung <soyoung.yang94@gmail.com>
2024-06-07 11:46:39 +0900

    Test: 엔티티 테스트 추가

--- src/test/java/com/sparta/springnewsfeed/SpringNewsfeedApplicationTests.java
+++ src/test/java/com/sparta/springnewsfeed/SpringNewsfeedApplicationTests.java
@@ -41,37 +41,51 @@ class SpringNewsfeedApplicationTests {
     void test() {
 
         // 사용자 db 등록
-        User user = new User();
-        user.setName("이름");
-        user.setUserid("aaa");
-        user.setPassword("1234");
-        user.setStatus(UserStatusEnum.VERIFIED);
-        userRepository.save(user);
+        User user1 = new User();
+        user1.setName("글쓴이");
+        user1.setUserid("aaa");
+        user1.setPassword("1234");
+        user1.setStatus(UserStatusEnum.VERIFIED);
+        userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setName("댓글러");
+        user2.setUserid("bbb");
+        user2.setPassword("1234");
+        user2.setStatus(UserStatusEnum.VERIFIED);
+        userRepository.save(user2);
+
+        User user3 = new User();
+        user3.setName("좋아요사냥꾼");
+        user3.setUserid("ccc");
+        user3.setPassword("1234");
+        user3.setStatus(UserStatusEnum.VERIFIED);
+        userRepository.save(user3);
 
         // 게시글 db 등록
         Post post = new Post();
         post.setTitle("게시글제목");
         post.setContent("게시글내용");
-        post.setUser(user);
+        post.setUser(user1);
         postRepository.save(post);
 
         // 댓글 db 등록
         Comment comment = new Comment();
         comment.setContent("댓글내용");
-        comment.setUser(user);
+        comment.setUser(user2);
         comment.setPost(post);
         commentRepository.save(comment);
 
         // 게시글 좋아요 db 등록
         PostLike postLike = new PostLike(
-                new PostLikeId(user.getId(), post.getId()),
-                user, post);
+                new PostLikeId(user3.getId(), post.getId()),
+                user3, post);
         postLikeRepository.save(postLike);
 
         // 댓글 좋아요 db 등록
         CommentLike commentLike = new CommentLike(
-                new CommentLikeId(user.getId(), comment.getId()),
-                user, comment);
+                new CommentLikeId(user3.getId(), comment.getId()),
+                user3, comment);
         commentLikeRepository.save(commentLike);
 
     }

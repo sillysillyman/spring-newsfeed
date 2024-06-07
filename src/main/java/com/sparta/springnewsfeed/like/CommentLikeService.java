f76917e97aa09485b0763d2bf5a82da1f4e96e0a
soyoung <soyoung.yang94@gmail.com>
2024-06-07 11:35:06 +0900

    fix: ResponseDto 수정
    - ResponseCode message 한글 변경
    - HttpStatusResponseDto를 ResposeCode enum으로 받아 처리

--- src/main/java/com/sparta/springnewsfeed/like/CommentLikeService.java
+++ src/main/java/com/sparta/springnewsfeed/like/CommentLikeService.java
@@ -3,6 +3,7 @@ package com.sparta.springnewsfeed.like;
 import com.sparta.springnewsfeed.comment.Comment;
 import com.sparta.springnewsfeed.comment.CommentRepository;
 import com.sparta.springnewsfeed.common.HttpStatusResponseDto;
+import com.sparta.springnewsfeed.common.ResponseCode;
 import com.sparta.springnewsfeed.user.User;
 import com.sparta.springnewsfeed.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,21 +25,21 @@ public class CommentLikeService {
         Comment comment = commentRepository.findById(commentId).orElse(null);
         User user = userRepository.findById(userId).orElse(null);
         if (comment == null || user == null) {
-            return new HttpStatusResponseDto(HttpStatus.BAD_REQUEST.toString(), "유효하지 않은 입력 값입니다.");
+            return new HttpStatusResponseDto(ResponseCode.INVALID_INPUT_VALUE);
         }
 
         // 예외 1) 이미 해당 userId-commentId 로 좋아요 등록되어있는 경우
         if (commentLikeRepository.findByIdUserIdAndIdCommentId(userId, commentId).orElse(null) != null) {
-            return new HttpStatusResponseDto(HttpStatus.BAD_REQUEST.toString(), "중복된 Entity입니다.");
+            return new HttpStatusResponseDto(ResponseCode.DUPLICATE_ENTITY);
         }
 
         // 예외 2) 자신의 댓글에 좋아요한 경우
         if (userId.equals(comment.getUser().getId())) {
-            return new HttpStatusResponseDto(HttpStatus.BAD_REQUEST.toString(), "자신의 댓글에 좋아요할 수 없습니다.");
+            return new HttpStatusResponseDto(ResponseCode.DO_NOT_LIKE_MY_COMMENT);
         }
 
         commentLikeRepository.save(new CommentLike(new CommentLikeId(userId, commentId), user, comment));
-        return new HttpStatusResponseDto(HttpStatus.OK.toString(), "요청이 성공했습니다.");
+        return new HttpStatusResponseDto(ResponseCode.SUCCESS);
     }
 
     // 댓글 좋아요 취소 (삭제)
@@ -46,10 +47,10 @@ public class CommentLikeService {
         // 사용자 요청 userId-commentId가 유효한지 검색
         CommentLike commentLike = commentLikeRepository.findByIdUserIdAndIdCommentId(userId, commentId).orElse(null);
         if (commentLike == null) {
-            return new HttpStatusResponseDto(HttpStatus.BAD_REQUEST.toString(), "유효하지 않은 입력 값입니다.");
+            return new HttpStatusResponseDto(ResponseCode.INVALID_INPUT_VALUE);
         }
 
         commentLikeRepository.delete(commentLike);
-        return new HttpStatusResponseDto(HttpStatus.OK.toString(), "요청이 성공했습니다.");
+        return new HttpStatusResponseDto(ResponseCode.SUCCESS);
     }
 }

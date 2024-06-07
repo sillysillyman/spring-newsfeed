ab623098061adbba67b7b6878ab5134efd645036
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-07 15:16:40 +0900

    feat: PostService에 updatePost 메서드 추가
    - PostRequest를 이용해 게시물 업데이트 기능 추가
    - 게시물의 제목과 내용을 변경할 수 있도록 구현

--- src/main/java/com/sparta/springnewsfeed/post/PostService.java
+++ src/main/java/com/sparta/springnewsfeed/post/PostService.java
@@ -4,6 +4,7 @@ import com.sparta.springnewsfeed.user.User;
 import com.sparta.springnewsfeed.user.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -12,11 +13,27 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
+    @Transactional
     public PostResponse createPost(PostRequest request) {
         User user = userRepository.findById(request.getUserId())
             .orElseThrow(() -> new RuntimeException("사용자를 찾지 못 했습니다."));
+
         Post post = new Post(request.getTitle(), request.getContent(), user);
-        
-        return new PostResponse(postRepository.save(post));
+        Post savedPost = postRepository.save(post);
+
+        return new PostResponse(savedPost);
+    }
+
+    @Transactional
+    public PostResponse updatePost(PostRequest request) {
+        Post post = postRepository.findById(request.getPostId())
+            .orElseThrow(() -> new RuntimeException("게시물을 찾지 못 했습니다."));
+
+        post.setTitle(request.getTitle());
+        post.setContent(request.getContent());
+
+        Post updatedPost = postRepository.save(post);
+
+        return new PostResponse(updatedPost);
     }
 }

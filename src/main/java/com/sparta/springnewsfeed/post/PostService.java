0a6480336aa5f574da2b6113347a5255fd0745a7
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-07 19:03:57 +0900

    feat: PostService에 게시물 조회 메서드 추가
    - getAllPosts: 모든 게시물을 조회하는 메서드 추가
    - getPostsByUserId: 특정 사용자의 게시물을 조회하는 메서드 추가
    - getPostsOfFollowees: 사용자가 팔로우한 사용자의 게시물을 조회하는 메서드 추가

--- src/main/java/com/sparta/springnewsfeed/post/PostService.java
+++ src/main/java/com/sparta/springnewsfeed/post/PostService.java
@@ -1,8 +1,12 @@
 package com.sparta.springnewsfeed.post;
 
+import com.sparta.springnewsfeed.follow.Follow;
 import com.sparta.springnewsfeed.user.User;
 import com.sparta.springnewsfeed.user.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +21,7 @@ public class PostService {
     @Transactional
     public PostResponse createPost(PostRequest request) {
         User user = userRepository.findById(request.getUserId())
-            .orElseThrow(() -> new RuntimeException("사용자를 찾지 못 했습니다."));
+            .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
 
         Post post = new Post(request.getTitle(), request.getContent(), user);
         Post savedPost = postRepository.save(post);
@@ -25,10 +29,43 @@ public class PostService {
         return new PostResponse(savedPost);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostResponse> getAllPosts() {
+        return postRepository.findAll().stream().map(PostResponse::new)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getPostsByUserId(Long userId) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        return postRepository.findByUser(user).stream().map(PostResponse::new)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getPostsOfFollowees(Long userId) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        List<Follow> followees = user.getFollowing();
+        List<PostResponse> postResponses = new ArrayList<>();
+        for (var followee : followees) {
+            User follwedUser = followee.getFollowed();
+            List<Post> posts = postRepository.findByUser(follwedUser);
+
+            for (Post post : posts) {
+                postResponses.add(new PostResponse(post));
+            }
+        }
+        return postResponses;
+    }
+
     @Transactional
     public PostResponse updatePost(PostRequest request) {
         Post post = postRepository.findById(request.getPostId())
-            .orElseThrow(() -> new RuntimeException("게시물을 찾지 못 했습니다."));
+            .orElseThrow(() -> new RuntimeException("게시물을 찾을 수 없습니다."));
 
         post.setTitle(request.getTitle());
         post.setContent(request.getContent());

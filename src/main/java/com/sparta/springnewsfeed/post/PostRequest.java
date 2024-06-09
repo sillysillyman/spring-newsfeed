81922559b824ed3065dd0295577a783f4e0952ee
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-10 06:34:21 +0900

    feat: PostRequest 클래스에 Setter 애너테이션 추가
    - PostRequest 클래스에 @Setter 애너테이션을 추가하여 필드 값을 설정할 수 있도록 수정

--- src/main/java/com/sparta/springnewsfeed/post/PostRequest.java
+++ src/main/java/com/sparta/springnewsfeed/post/PostRequest.java
@@ -3,8 +3,10 @@ package com.sparta.springnewsfeed.post;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class PostRequest {
 
     @NotNull

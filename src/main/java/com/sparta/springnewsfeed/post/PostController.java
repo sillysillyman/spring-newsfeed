cb27672b5d4fc1367c9c398afac1460739734b05
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-10 00:45:48 +0900

    feat: PostController에 RequestMapping 추가
    - PostController 클래스에 "/posts" 경로로 매핑하는 @RequestMapping 애너테이션 추가

--- src/main/java/com/sparta/springnewsfeed/post/PostController.java
+++ src/main/java/com/sparta/springnewsfeed/post/PostController.java
@@ -1,10 +1,12 @@
 package com.sparta.springnewsfeed.post;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
 
     public final PostService postService;

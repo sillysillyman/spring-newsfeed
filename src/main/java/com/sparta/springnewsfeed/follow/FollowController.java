ab96d07b922f55929db3ed6075942cce475e47a2
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-06-10 02:58:37 +0900

    feat: 팔로우 하기 컨트롤러 구현

---
+++ src/main/java/com/sparta/springnewsfeed/follow/FollowController.java
@@ -0,0 +1,31 @@
+package com.sparta.springnewsfeed.follow;
+
+import com.sparta.springnewsfeed.common.HttpStatusResponseDto;
+import com.sparta.springnewsfeed.common.ResponseCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class FollowController {
+
+    private final FollowService followService;
+
+    @Autowired
+    public FollowController(FollowService followService) {
+        this.followService = followService;
+    }
+
+    // 팔로우 하기
+    @PostMapping("/{followerUserid}/follow/{followedUserid}")
+    public ResponseEntity<HttpStatusResponseDto> followUser(
+            @PathVariable String followerUserid, @PathVariable String followedUserid) {
+        ResponseCode responseCode = followService.followUser(followerUserid, followedUserid);
+        HttpStatusResponseDto response = new HttpStatusResponseDto(responseCode);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(responseCode.getStatusCode()));
+    }
+
+
+}

2976ad7426870830dcc38d3a4c034fc440374d11
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-06-10 00:26:51 +0900

    feat: 유저 조회 응답 DTO 구현

---
+++ src/main/java/com/sparta/springnewsfeed/user/UserInquiryResponseDto.java
@@ -0,0 +1,14 @@
+package com.sparta.springnewsfeed.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInquiryResponseDto {
+
+    private String userid;
+    private String name;
+    private String introduction;
+    private String email;
+}

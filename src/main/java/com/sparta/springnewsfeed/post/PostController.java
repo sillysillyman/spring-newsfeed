74fa0b63aa7fd1150b47f2e5361d8c399c2ad872
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-10 06:36:04 +0900

    feat: PostController에 게시물 생성 엔드포인트 추가
    - @PostMapping 애너테이션을 사용하여 게시물 생성 엔드포인트 추가
    - createPost 메서드에서 @RequestParam으로 userId를 받고, @RequestBody로 PostRequest 객체를 받아 처리
    - PostRequest 객체에 userId를 설정하고, PostService의 createPost 메서드를 호출하여 게시물 생성

--- src/main/java/com/sparta/springnewsfeed/post/PostController.java
+++ src/main/java/com/sparta/springnewsfeed/post/PostController.java
@@ -1,7 +1,12 @@
 package com.sparta.springnewsfeed.post;
 
+import com.sparta.springnewsfeed.common.HttpStatusResponseDto;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     public final PostService postService;
+
+    @PostMapping
+    public HttpStatusResponseDto createPost(@RequestParam Long userId,
+        @RequestBody @Valid PostRequest request) {
+        request.setUserId(userId);
+        return postService.createPost(request);
+    }
 }

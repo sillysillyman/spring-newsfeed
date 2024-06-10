126f15cb497b6c8b794d7aa09d7424099395694f
soyoung <soyoung.yang94@gmail.com>
2024-06-10 13:07:10 +0900

    feat: PostLike & CommentLike 레포지토리 쿼리메소드 추가
    - exist 쿼리메소드 추가 후 활용

--- src/main/java/com/sparta/springnewsfeed/like/CommentLikeRepository.java
+++ src/main/java/com/sparta/springnewsfeed/like/CommentLikeRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, CommentLikeId> {
     Optional<CommentLike> findByIdUserIdAndIdCommentId(Long userId, Long commentId);
+    boolean existsByIdUserIdAndIdCommentId(Long userId, Long commentId);
 }

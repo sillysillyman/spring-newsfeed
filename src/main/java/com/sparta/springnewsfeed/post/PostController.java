e869457f93221dcec79713a47515dc2922049c6c
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-10 15:01:12 +0900

    refactor: PostController에 JWT 인증 및 검증 로직 추가
    - 모든 PostController 메서드에 HttpServletRequest를 추가하여 JWT 토큰 검증 로직 구현
    - Post 생성, 업데이트, 삭제 시 HttpServletRequest에서 토큰을 추출하여 사용자 인증 처리
    - 각 메서드에서 jwtTokenProvider를 통해 토큰 검증 로직 추가
    - 기존의 URL 경로 형식 변경

--- src/main/java/com/sparta/springnewsfeed/post/PostController.java
+++ src/main/java/com/sparta/springnewsfeed/post/PostController.java
@@ -1,8 +1,9 @@
 package com.sparta.springnewsfeed.post;
 
 import com.sparta.springnewsfeed.common.HttpStatusResponseDto;
-import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import com.sparta.springnewsfeed.security.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,48 +11,56 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
-@RequestMapping("/posts")
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class PostController {
 
-    public final PostService postService;
+    private final PostService postService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @PostMapping
-    public HttpStatusResponseDto createPost(@RequestParam Long userId,
-        @RequestBody @Valid PostRequest request) {
-        request.setUserId(userId);
-        return postService.createPost(request);
+    @PostMapping("/{userId}/posts")
+    public HttpStatusResponseDto createPost(@PathVariable Long userId,
+        @RequestBody PostRequest postRequest, HttpServletRequest request) {
+        String token = jwtTokenProvider.extractToken(request);
+        return postService.createPost(token, userId, postRequest);
     }
 
-    @GetMapping
+    @GetMapping("/posts")
     public HttpStatusResponseDto getAllPosts() {
         return postService.getAllPosts();
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}/following/posts")
+    public HttpStatusResponseDto getPostsOfFollowees(@PathVariable Long userId,
+        HttpServletRequest request) {
+        String token = jwtTokenProvider.extractToken(request);
+        return postService.getPostsOfFollowees(token, userId);
+    }
+
+    @GetMapping("/{userId}/posts")
     public HttpStatusResponseDto getPostsByUserId(@PathVariable Long userId) {
         return postService.getPostsByUserId(userId);
     }
 
-    @GetMapping("/users/{userId}/following")
-    public HttpStatusResponseDto getPostsOfFollowees(@PathVariable Long userId) {
-        return postService.getPostsOfFollowees(userId);
+    @GetMapping("/{userId}/posts/{postId}")
+    public HttpStatusResponseDto getPostById(@PathVariable Long userId, @PathVariable Long postId) {
+        return postService.getPostById(userId, postId);
     }
 
-    @PutMapping("/{postId}")
-    public HttpStatusResponseDto updatePost(@RequestParam Long userId, @PathVariable Long postId,
-        @RequestBody @Valid PostRequest postRequest) {
-        postRequest.setUserId(userId);
-        postRequest.setPostId(postId);
-        return postService.updatePost(postRequest);
+    @PutMapping("/{userId}/posts/{postId}")
+    public HttpStatusResponseDto updatePost(@PathVariable Long userId, @PathVariable Long postId,
+        @RequestBody PostRequest postRequest, HttpServletRequest request) {
+        String token = jwtTokenProvider.extractToken(request);
+        return postService.updatePost(token, userId, postId, postRequest);
     }
 
-    @DeleteMapping("/{postId}")
-    public HttpStatusResponseDto deletePost(@RequestParam Long userId, @PathVariable Long postId) {
-        return postService.deletePost(userId, postId);
+    @DeleteMapping("/{userId}/posts/{postId}")
+    public HttpStatusResponseDto deletePost(@PathVariable Long userId, @PathVariable Long postId,
+        HttpServletRequest request) {
+        String token = jwtTokenProvider.extractToken(request);
+        return postService.deletePost(token, userId, postId);
     }
 }

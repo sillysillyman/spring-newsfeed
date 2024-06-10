66cb205d7fc877c29c0d6463f615efc6def5e7fc
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-10 13:02:06 +0900

    feat: UserPrincipal 클래스 추가
    - User 엔티티를 기반으로 한 UserDetails 구현 클래스 추가
    - UserPrincipal 클래스를 통해 Spring Security의 UserDetails 인터페이스 구현
    - 사용자 ID, 비밀번호, 권한 등의 사용자 세부 정보를 제공하는 메서드 구현
    - 사용자 계정의 만료, 잠금, 자격 증명 만료, 활성화 상태를 항상 true로 반환
    - create 메서드를 통해 User 객체를 UserPrincipal 객체로 변환하는 유틸리티 메서드 추가

---
+++ src/main/java/com/sparta/springnewsfeed/security/UserPrincipal.java
@@ -0,0 +1,64 @@
+package com.sparta.springnewsfeed.security;
+
+import com.sparta.springnewsfeed.user.User;
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    public static UserPrincipal create(User user) {
+        return new UserPrincipal(user);
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    public String getUserId() {
+        return user.getUserId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

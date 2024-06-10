7f0979cef278344144dcb7bb7f68a29684d687cf
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-10 11:26:59 +0900

    feat: PostController에 게시물 수정 및 삭제 엔드포인트 추가
    - @PutMapping 애너테이션을 사용하여 게시물 수정 엔드포인트 추가
    - @DeleteMapping 애너테이션을 사용하여 게시물 삭제 엔드포인트 추가
    - updatePost 메서드에서 게시물 수정 로직 구현
    - deletePost 메서드에서 게시물 삭제 로직 구현

--- src/main/java/com/sparta/springnewsfeed/post/PostController.java
+++ src/main/java/com/sparta/springnewsfeed/post/PostController.java
@@ -3,9 +3,11 @@ package com.sparta.springnewsfeed.post;
 import com.sparta.springnewsfeed.common.HttpStatusResponseDto;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,5 +40,18 @@ public class PostController {
     @GetMapping("/users/{userId}/following")
     public HttpStatusResponseDto getPostsOfFollowees(@PathVariable Long userId) {
         return postService.getPostsOfFollowees(userId);
+    }
+
+    @PutMapping("/{postId}")
+    public HttpStatusResponseDto updatePost(@RequestParam Long userId, @PathVariable Long postId,
+        @RequestBody @Valid PostRequest postRequest) {
+        postRequest.setUserId(userId);
+        postRequest.setPostId(postId);
+        return postService.updatePost(postRequest);
+    }
+
+    @DeleteMapping("/{postId}")
+    public HttpStatusResponseDto deletePost(@RequestParam Long userId, @PathVariable Long postId) {
+        return postService.deletePost(userId, postId);
     }
 }

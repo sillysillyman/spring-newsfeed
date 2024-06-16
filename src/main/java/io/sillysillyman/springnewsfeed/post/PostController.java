28999491627d467cbc99c2c4e1e52e1cdc3639b2
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-17 07:36:00 +0900

    refactor: PostController 리팩토링 및 경로 수정
    - PostController 클래스에서 JwtUtil 종속성 제거
    - 매핑 경로 단순화
    - createPost 메서드에서 PostRequestDto를 사용하여 게시글 생성
    - updatePost 및 deletePost 메서드에 username 매개변수 추가

--- src/main/java/io/sillysillyman/springnewsfeed/post/PostController.java
+++ src/main/java/io/sillysillyman/springnewsfeed/post/PostController.java
@@ -1,14 +1,11 @@
 package io.sillysillyman.springnewsfeed.post;
 
-import io.sillysillyman.springnewsfeed.auth.JwtUtil;
 import io.sillysillyman.springnewsfeed.auth.UserDetailsImpl;
 import io.sillysillyman.springnewsfeed.common.HttpStatusResponseDto;
 import io.sillysillyman.springnewsfeed.post.dto.PostRequestDto;
 import io.sillysillyman.springnewsfeed.post.dto.PostResponseDto;
 import io.sillysillyman.springnewsfeed.user.User;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,66 +13,57 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 @RequiredArgsConstructor
 public class PostController {
 
-    @Autowired
     private final PostService postService;
-    private final JwtUtil jwtUtil;
 
-
-    @PostMapping("/posts")
+    @PostMapping
     public HttpStatusResponseDto<PostResponseDto> createPost(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
-        @RequestBody Post post) {
-        // 현재 인증된 사용자를 가져와 게시글의 작성자로 설정
-        User author = userDetails.getUser();
-        post.setUser(author);
-        // 게시글 저장
-        return postService.createPost(post);
+        @RequestBody PostRequestDto postRequestDto) {
+        User user = userDetails.getUser();
+        return postService.createPost(user, postRequestDto);
     }
 
-    @GetMapping("/posts")
+    @GetMapping
     public HttpStatusResponseDto<?> getAllPosts() {
         return postService.getAllPosts();
     }
 
-    @GetMapping("/{username}/following/posts")
-    public HttpStatusResponseDto<?> getPostsOfFollowees(@PathVariable String username,
-        HttpServletRequest request) {
-        String token = jwtUtil.getAccessTokenFromHeader(request);
-        return postService.getPostsOfFollowees(token, username);
+    @GetMapping("/{username}/following")
+    public HttpStatusResponseDto<?> getPostsOfFollowees(@PathVariable String username) {
+        return postService.getPostsOfFollowees(username);
     }
 
-    @GetMapping("/{username}/posts")
+    @GetMapping("/{username}")
     public HttpStatusResponseDto<?> getPostsByUsername(@PathVariable String username) {
         return postService.getPostsByUsername(username);
     }
 
-    @GetMapping("/{username}/posts/{postId}")
+    @GetMapping("/{username}/{postId}")
     public HttpStatusResponseDto<?> getPostById(@PathVariable String username,
         @PathVariable Long postId) {
         return postService.getPostById(username, postId);
     }
 
-    @PutMapping("/posts/{postId}")
-    public HttpStatusResponseDto<?> updatePost(@PathVariable Long postId,
-        @RequestBody PostRequestDto postRequestDto,
-        @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
+    @PutMapping("/{username}/{postId}")
+    public HttpStatusResponseDto<?> updatePost(@PathVariable String username,
+        @PathVariable Long postId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestBody PostRequestDto postRequestDto) {
         User user = userDetails.getUser();
-        return postService.updatePost(user, postId, postRequestDto);
+        return postService.updatePost(username, postId, user, postRequestDto);
     }
 
-    @DeleteMapping("/posts/{postId}")
-    public HttpStatusResponseDto<Void> deletePost(@PathVariable Long postId,
+    @DeleteMapping("/{username}/{postId}")
+    public HttpStatusResponseDto<Void> deletePost(@PathVariable String username,
+        @PathVariable Long postId,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         User user = userDetails.getUser();
-        return postService.deletePost(user, postId);
+        return postService.deletePost(username, postId, user);
     }
 }

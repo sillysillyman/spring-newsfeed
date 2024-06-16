c852aa7fc4d452e80ecff20a7809b0257c3f9c7e
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-17 07:30:42 +0900

    refactor: Spring Security 설정 및 변수명 개선
    - AbstractHttpConfigurer를 사용하여 CSRF 설정을 disable로 변경
    - 요청 경로와 메서드별 접근 권한 설정을 더 간결하게 개선
    - 변수명 개선: http -> httpSecurity 등
    - 인증 필터 설정 순서를 수정하여 정확한 필터링 적용 보장

--- src/main/java/io/sillysillyman/springnewsfeed/auth/WebSecurityConfig.java
+++ src/main/java/io/sillysillyman/springnewsfeed/auth/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,16 +26,19 @@ public class WebSecurityConfig {
     private final UserRepository userRepository;
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration)
         throws Exception {
-        return configuration.getAuthenticationManager();
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, userRepository);
-        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
-        return filter;
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtil,
+            userRepository);
+        jwtAuthenticationFilter.setAuthenticationManager(
+            authenticationManager(authenticationConfiguration));
+        return jwtAuthenticationFilter;
     }
 
     @Bean
@@ -43,35 +47,33 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         // CSRF 설정
-        http.csrf((csrf) -> csrf.disable());
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
         // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
-        http.sessionManagement((sessionManagement) ->
+        httpSecurity.sessionManagement((sessionManagement) ->
             sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
 
-        http.authorizeHttpRequests((authorizeHttpRequests) ->
+        httpSecurity.authorizeHttpRequests((authorizeHttpRequests) ->
             authorizeHttpRequests
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .permitAll() // resources 접근 허용 설정
-                .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                .requestMatchers("/error").permitAll()
-                .requestMatchers("/users/signup", "/users/login").permitAll() //
-                .requestMatchers("/email-verification/send", "/email-verification/resend",
-                    "/email-verification/verify").permitAll() // 이메일 인증 관련 요청 허가
-                .requestMatchers(HttpMethod.GET, "/users/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/users/**").authenticated()
-                .requestMatchers(HttpMethod.PUT, "/users/**").authenticated()
-                .requestMatchers(HttpMethod.DELETE, "/users/**").authenticated()
+                .requestMatchers("/", "/error").permitAll() // 메인 페이지 및 에러 페이지 요청 허가
+                .requestMatchers("/users/signup", "/users/login").permitAll() // 회원가입 및 로그인 요청 허가
+                .requestMatchers("/email-verification/**").permitAll() // 이메일 인증 관련 요청 허가
+                .requestMatchers(HttpMethod.GET, "/**").permitAll()
                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         // 필터 관리
-        http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        // httpSecurity.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(jwtAuthorizationFilter(),
+            UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter(),
+            UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
+        return httpSecurity.build();
     }
 }

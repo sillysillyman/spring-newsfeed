3c3b4d40619032606c52c69045dd24e5f7766229
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-19 02:33:32 +0900

    refactor: UpdateProfileRequestDto 필드 수정 및 유효성 검사 추가
    - username 필드 제거
    - introduction 필드에 @NotNull 애너테이션 추가 및 최대 길이 100자로 설정
    - pictureUrl 필드 추가 및 @Pattern 애너테이션을 사용하여 유효한 URL 형식 검사 추가
      - 정규 표현식을 사용하여 http 또는 https로 시작하는 유효한 URL 형식 확인
    - 각 유효성 검사 애너테이션에 메시지 수정 및 추가
      - "소개글은 최대 100글자까지 입력 가능합니다."
      - "유효한 URL 형식이어야 합니다."

--- src/main/java/io/sillysillyman/springnewsfeed/user/dto/UpdateProfileRequestDto.java
+++ src/main/java/io/sillysillyman/springnewsfeed/user/dto/UpdateProfileRequestDto.java
@@ -1,5 +1,7 @@
 package io.sillysillyman.springnewsfeed.user.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,9 +10,10 @@ import lombok.Setter;
 @Setter
 public class UpdateProfileRequestDto {
 
-    @Size(max = 10, message = "이름은 최대 10글자까지 입력 가능합니다.")
-    private String username;
-
-    @Size(max = 20, message = "한줄소개는 최대 20글자까지 입력 가능합니다.")
+    @Size(max = 100, message = "소개글은 최대 100글자까지 입력 가능합니다.")
+    @NotNull
     private String introduction;
+
+    @Pattern(regexp = "^(https?|ftp)://[^\\s/$.?#].[^\\s]*$", message = "유효한 URL 형식이어야 합니다.")
+    private String pictureUrl;
 }

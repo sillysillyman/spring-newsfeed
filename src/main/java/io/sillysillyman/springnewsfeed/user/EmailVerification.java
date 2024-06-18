3228cc9768581f0221ab9c3b856cc31530a73a07
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-18 17:48:14 +0900

    refactor: EmailVerification 엔티티에서 Timestamped 상속 제거 및 JPA 감사 기능 제거
    - EmailVerification 클래스에서 Timestamped 상속을 제거하여 더 이상 createdAt과 updatedAt 필드가 포함되지 않음
    - @EntityListeners(AuditingEntityListener.class) 애너테이션 제거

--- src/main/java/io/sillysillyman/springnewsfeed/user/EmailVerification.java
+++ src/main/java/io/sillysillyman/springnewsfeed/user/EmailVerification.java
@@ -1,9 +1,7 @@
 package io.sillysillyman.springnewsfeed.user;
 
-import io.sillysillyman.springnewsfeed.common.Timestamped;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,15 +12,13 @@ import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "email_verification")
-public class EmailVerification extends Timestamped {
+public class EmailVerification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

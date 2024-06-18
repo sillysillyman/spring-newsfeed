7ba462ea3275a62a4e58947dc8718d436d0d19f0
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-19 01:47:43 +0900

    refactor: EmailVerificationRequestDto에 유효성 검사 추가
    - @Pattern 애너테이션을 사용하여 code 필드에 대한 유효성 검사 추가
      - 정규 표현식: 숫자 6자리
      - 메시지: "인증 코드는 6자리 숫자이어야 합니다."

--- src/main/java/io/sillysillyman/springnewsfeed/user/dto/EmailVerificationRequestDto.java
+++ src/main/java/io/sillysillyman/springnewsfeed/user/dto/EmailVerificationRequestDto.java
@@ -1,6 +1,7 @@
 package io.sillysillyman.springnewsfeed.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,6 @@ import lombok.Setter;
 public class EmailVerificationRequestDto {
 
     @NotBlank(message = "인증 코드를 입력해주세요.")
+    @Pattern(regexp = "\\d{6}", message = "인증 코드는 6자리 숫자이어야 합니다.")
     private String code;
 }
